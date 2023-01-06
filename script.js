b2ec4ac487efee26b864fbed63b00e533f085034
Tyler <tpiwowarski3@gatech.edu>
2023-01-06 10:50:00 -0500

    Created basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,20 @@
+/* ARITHMETIC FUNCTIONS */
+// Add function
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// Subtract function
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+// Multiply function
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Divide function
+function divide(num1, num2) {
+    return num1 / num2;
+}
