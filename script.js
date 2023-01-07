dea0aeb6572435373b85b8a8e6fa69a866dc2bfc
Tyler <tpiwowarski3@gatech.edu>
2023-01-06 19:38:23 -0500

    Finished UI

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 /* ARITHMETIC FUNCTIONS */
-// Add function
+// Adds 2 numbers
 function add(num1, num2) {
     return num1 + num2;
 }
 
-// Subtract function
+// Subtracts 2 numbers
 function subtract(num1, num2) {
     return num1 - num2;
 }
 
-// Multiply function
+// Multiplies 2 numbers
 function multiply(num1, num2) {
     return num1 * num2;
 }
 
-// Divide function
+// Divides 2 numbers 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+/* OPERATOR FUNCTION */
+// Takes 2 numbers and an operand
+function operate(num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    }
+    else if (operator === '-') {
+        return subtract(num1, num2);
+    }
+    else if (operator === '*') {
+        return multiply(num1, num2);
+    }
+    else if (operator === '/') {
+        return divide(num1, num2);
+    }
 }
